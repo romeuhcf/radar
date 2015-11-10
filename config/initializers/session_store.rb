1fda92ac1777ea97b7139d5db7e6cd43a4759d62
RomeuFonseca <romeu.hcf@gmail.com>
2015-11-10 10:51:40 -0200

    Fix store config for test

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-redis_options = Rails.configuration.database_configuration[Rails.env]['redis'].symbolize_keys
+redis_options = (Rails.configuration.database_configuration[Rails.env]['redis'] || {}) .symbolize_keys
 redis_options[:namespace] = [redis_options[:namespace], 'session'].compact.join(':')
 redis_options[:expires_in] = 90.minutes
 Rails.application.config.session_store = :redis_store, redis_options
