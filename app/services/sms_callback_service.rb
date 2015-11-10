7ea0437f8c5ed83eeffb85ba7280014af22109a5
Romeu Fonseca <romeu.hcf@gmail.com>
2015-11-10 00:52:37 -0200

    Git fix callback service --skip-ci

--- app/services/sms_callback_service.rb
+++ app/services/sms_callback_service.rb
@@ -1,10 +1,8 @@
 class SmsCallbackService
   def perform(params)
-    the_provider  = BaseProvider.providers.find{|provider| provider.my_callback?(params) }
-    the_status    = the_provider.interpret_callback(params)
-
-    the_localizer = Localizer.get_item(the_status.uid)
-    the_message   = the_localizer.item
+    the_provider = BaseProvider.providers.find{|provider| provider.my_callback?(params) }
+    the_status   = the_provider.interpret_callback(params)
+    the_message  = Localizer.get_item(the_status.uid)
 
     the_message.update_transmission_result(the_status)
   end
