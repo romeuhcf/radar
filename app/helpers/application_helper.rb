1a0cc304b04cf3c8007ca8135e7e27cff85f431c
Romeu Fonseca <romeu.hcf@gmail.com>
2015-11-22 00:35:05 -0200

    Better layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,17 @@ module ApplicationHelper
   def footer_js
     content_for :footer_js
   end
+
+  def simple_form_horizontal_setup
+    {
+      wrapper: :horizontal_form,
+      wrapper_mappings: {
+        check_boxes: :horizontal_radio_and_checkboxes,
+        radio_buttons: :horizontal_radio_and_checkboxes,
+        file: :horizontal_file_input,
+        boolean: :horizontal_boolean
+      }
+    }
+  end
+
 end
